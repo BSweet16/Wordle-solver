18055ae571b72243691d7fb056b5a894b301bf34
Blake Sweet <blake.sweet16@gmail.com>
2022-02-16 01:12:25 -0500

    Load a dictionary of words to search through

---
+++ Class/solver.py
@@ -0,0 +1,23 @@
+from .puzzle import MyPuzzle
+from .Word import MyWord
+from english_words import english_words_set
+
+# =============================================
+# This class utilizes a python dictionary library.
+# enlish-words-py: https://pypi.org/project/english-words/
+# =============================================
+class PuzzleSolver(object):
+    def SolvePuzzle(self):
+        dictionary = self.buildWordList()
+        print(dictionary)
+        
+    # =============================================
+    # Build a list of words to use as a dictionary. 
+    # This list only includes the words that are the length of the target word size for the puzzle
+    # =============================================
+    def buildWordList(self):
+        foundWords = []
+        for word in english_words_set:
+            if len(word) == MyWord.MAX_WORD_LENGTH: # Build a list of words that can apply to the max word length
+                foundWords.append(word)
+        return foundWords
--- Wordle_solver.py
+++ Wordle_solver.py
@@ -1,16 +1,46 @@
 from Class.puzzle import MyPuzzle
 from Class.Word import MyWord
+from Class.solver import PuzzleSolver
+
+def MainMenu():
+    print("Which would you like to do? (Choose a number)")
+    print("1: Play Wordle")
+    print("2: Solve a Wordle")
+    return input(">> ")
+
+def PlayGame():
+    # Welcome message
+    print("Welcome to Wordle!")
+
+    # Create a puzzle with an answer
+    daily_puzzle = MyPuzzle(MyWord("Exams"))
+
+    # Output the answer letters to the puzzle
+    # print(f"Answer today: {daily_puzzle.answer.word}")
+
+    # Attempt a word
+    # attempt1 = daily_puzzle.playWord("Blaek")
+    # print(f"Attempt 1: {attempt1}")
+
+    return
+
+
+def SolvePuzzle():
+    PuzzleSolver().SolvePuzzle()
+
+    return
 
 def main():
-    # Create a puzzle with an answer
-    daily_puzzle = MyPuzzle(MyWord())
-    
-    # Output the answer letters to the puzzle
-    print(f"Answer today: {daily_puzzle.answer.word}")
-    
-    # Attempt a word
-    attempt1 = daily_puzzle.playWord("Blaek")
-    print(f"Attempt 1: {attempt1}")
+    user_input = 0
+    while user_input != 1 and user_input != 2:
+        user_input = MainMenu()
+        if user_input == "1":
+            PlayGame()
+        elif user_input == "2":
+            SolvePuzzle()
+
+        return
+    return
 
 if __name__ == "__main__":
     main()
