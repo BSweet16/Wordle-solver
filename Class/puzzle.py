db596fea36a3df5e023ca21f565d698e415b766d
Blake Sweet <blake.sweet16@gmail.com>
2022-02-15 23:13:35 -0500

    Added a method to attempt a guess to the answer of the puzzle

--- Class/puzzle.py
+++ Class/puzzle.py
@@ -1,9 +1,38 @@
 from .Word import MyWord
+from .AttemptResult import AttemptResult
 
 class MyPuzzle(object):
+    MAX_ATTEMPTS = 6
 
     # =============================================
     # Constructor
     # =============================================
     def __init__(self, my_answer):
-        self.answer = my_answer if my_answer else MyWord() # Set the answer to the puzzle
+        self.answer = my_answer if my_answer else MyWord()      # The answer to the puzzle
+        self.usedAttempts = 0                                   # The number of attempts used
+
+    # =============================================
+    # Used to attempt to solve the puzzle with a word
+    # =============================================
+    def playWord(self, guess):
+        # Check if an attempt is able to be used
+        if self.usedAttempts >= self.MAX_ATTEMPTS:
+            print("Unable to attempt answer. Maximum number of guesses reached.")
+            return []
+        
+        matching_characters = []
+        index = 0
+
+        # Create a parallel array of results (parallel to the characters in the string)
+        for character in guess:
+            if character.upper() == self.answer.word[index].upper():        # Character exists in this position
+                matching_characters.append(AttemptResult.EXACT.value)
+            elif guess.find(character.upper()) > -1:                        # Character exists somewhere else in the answer
+                matching_characters.append(AttemptResult.WRONG_POSITION.value)
+            else:
+                matching_characters.append(AttemptResult.WRONG.value)             # Character does not exist at all
+            index += 1
+
+        self.usedAttempts += 1
+        
+        return matching_characters
--- Wordle_solver.py
+++ Wordle_solver.py
@@ -3,10 +3,14 @@ from Class.Word import MyWord
 
 def main():
     # Create a puzzle with an answer
-    daily_puzzle = MyPuzzle(MyWord("Blake"))
+    daily_puzzle = MyPuzzle(MyWord())
     
     # Output the answer letters to the puzzle
     print(f"Answer today: {daily_puzzle.answer.word}")
+    
+    # Attempt a word
+    attempt1 = daily_puzzle.playWord("Blaek")
+    print(f"Attempt 1: {attempt1}")
 
 if __name__ == "__main__":
     main()
