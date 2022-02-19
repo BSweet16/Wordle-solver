58c90b7d09a10bd227a5e3dac54ac825189e5e83
Blake Sweet <blake.sweet16@gmail.com>
2022-02-19 04:42:39 -0500

    Added pattern to solve word

--- Class/puzzle.py
+++ Class/puzzle.py
@@ -19,11 +19,21 @@ class MyPuzzle(object):
         if self.usedAttempts >= self.MAX_ATTEMPTS:
             print("Unable to attempt answer. Maximum number of guesses reached.")
             return []
-        
+
+        # Determine how the answer compared to the real answer
+        print(f"Attempt {self.usedAttempts + 1}: {guess}")
+        if len(self.answer.word) > 0:
+            return self.compareKnownAnswer(guess)
+        else:
+            return self.compareUnknownAnswer(guess)
+
+    # =============================================
+    # Used to attempt to solve the puzzle with a word when the answer is known
+    # =============================================
+    def compareKnownAnswer(self, guess):
+        # Create a parallel array of results (parallel to the characters in the string)
         matching_characters = []
         index = 0
-
-        # Create a parallel array of results (parallel to the characters in the string)
         for character in guess:
             if character.upper() == self.answer.word[index].upper():        # Character exists in this position
                 matching_characters.append(AttemptResult.EXACT.value)
@@ -36,3 +46,24 @@ class MyPuzzle(object):
         self.usedAttempts += 1
         
         return matching_characters
+
+    # =============================================
+    # Used to attempt to solve the puzzle with a word
+    # =============================================
+    def compareUnknownAnswer(self, guess):
+        # Display legend
+        print("Enter the following values for each letter:")
+        print(f"{AttemptResult.EXACT.value} (Green): Letter was correct")
+        print(f"{AttemptResult.WRONG_POSITION.value} (Yellow): Letter is in another position of the word")
+        print(f"{AttemptResult.WRONG.value} (Red/Grey): Letter does not exist in the word")
+
+        # Enter number result of each value
+        attempt_results = []
+        for character in guess:
+            attempt_results.append(input(f"\t{character}: "))
+
+        # Complete a usage
+        self.usedAttempts += 1
+
+        # Return number result array
+        return attempt_results
--- Class/solver.py
+++ Class/solver.py
@@ -1,5 +1,6 @@
 from .puzzle import MyPuzzle
 from .Word import MyWord
+from .AttemptResult import AttemptResult
 from english_words import english_words_set
 
 # =============================================
@@ -7,9 +8,38 @@ from english_words import english_words_set
 # enlish-words-py: https://pypi.org/project/english-words/
 # =============================================
 class PuzzleSolver(object):
+    puzzle = MyPuzzle("")
+
+    # =============================================
+    # This function is responsible for the organization of calling all 
+    # methods needed to solve the puzzle. 
+    # =============================================
     def SolvePuzzle(self):
-        dictionary = self.buildWordList()
-        print(dictionary)
+        self.dictionary = self.buildWordList()
+        print()
+
+        # Attempt to solve the puzzle 
+        puzzle_solved = False
+        while((self.puzzle.usedAttempts < self.puzzle.MAX_ATTEMPTS) and puzzle_solved == False): # Continue until out of guesses or the puzzle is solved
+            attempt_results = self.playBasicAttempt() # Attempt a guess and filter the results accordingly
+
+            # Determine if the correct answer has been found
+            correct_answer = True
+            for attempt_result in attempt_results:
+                if attempt_result != AttemptResult.EXACT.value:
+                    correct_answer = False
+
+            # Output success message!
+            if correct_answer == True:
+                puzzle_solved = correct_answer
+                print(f"Puzzle solved in {self.puzzle.usedAttempts} attempts")
+                return
+
+        # Output failure message
+        print("Unable to solve puzzle... ")
+        print(f"Remaining options: {self.dictionary}")
+        return
+        
         
     # =============================================
     # Build a list of words to use as a dictionary. 
@@ -21,3 +51,35 @@ class PuzzleSolver(object):
             if len(word) == MyWord.MAX_WORD_LENGTH: # Build a list of words that can apply to the max word length
                 foundWords.append(word.upper())
         return foundWords
+
+    # =============================================
+    # Attempts to solve the puzzle with a word
+    # This is the most basic method to attempt to solve the puzzle
+    # =============================================
+    def playBasicAttempt(self):
+        # Make an attempt of a word
+        attempted_word = self.dictionary[0]
+        matching_characters = self.puzzle.playWord(attempted_word) # Uses first word in the known dictionary
+
+        # Filter the dictonary according to the results
+        index = 0 # The index of the current letter in the guessed word being evaluated
+        for attempted_letter in attempted_word:
+            if matching_characters[index] == AttemptResult.EXACT.value: # Keep words with this letter in this position
+                for word in self.dictionary:
+                    if word[index] != attempted_letter: # If the letters DON'T match, remove the word. They should match, according to the attempt result
+                        self.dictionary.remove(word)
+            elif matching_characters[index] == AttemptResult.WRONG_POSITION.value: # Remove words with letter in this position
+                for word in self.dictionary:
+                    if word[index] == attempted_letter: # If the letters DO match, remove the word. They should not match, according to the attempt result
+                        self.dictionary.remove(word)
+            elif matching_characters[index] == AttemptResult.WRONG.value: # Remove words with this letter
+                for word in self.dictionary:
+                    if attempted_letter in word: # If the letter exists in the word, remove the word. It shoudl not exist in the word at all, according to the attempt result
+                        self.dictionary.remove(word)
+            else: 
+                print("Error: Discovered unrecognized attempt result value while filtering")
+
+            index += 1
+        print(f"Remaining options: {self.dictionary}")
+        return matching_characters
+        
